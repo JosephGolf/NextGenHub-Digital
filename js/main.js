048566c20445f0cbf1a2b50e1ac98c49445fc836
josephnma <josgolf@yahoo.ca>
2023-08-12 22:55:59 +0100

    added new js folder to root

---
+++ js/main.js
@@ -0,0 +1,124 @@
+$(document).ready(function(){
+
+     $('.fa-bars').click(function(){
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+    });
+
+    $(window).on('load scroll',function(){
+        $('.fa-bars').removeClass('fa-times');
+        $('.navbar').removeClass('nav-toggle');
+
+        if($(window).scrollTop()>35)
+        {
+            $('.header').css({'background':'#002e5f','box-shadow':'0 .2rem .5rem rgba(0,0,0,.4)'});
+        }
+        else
+        {
+            $('.header').css({'background':'none','box-shadow':'none'});
+        }
+    });
+
+    const counters = document.querySelectorAll('.counter');
+    const speed = 120;
+    counters.forEach(counter => {
+	const updateCount = () => {
+		const target = +counter.getAttribute('data-target');
+		const count = +counter.innerText;
+		const inc = target / speed;
+		if (count < target) {
+			counter.innerText = count + inc;
+			setTimeout(updateCount, 1);
+		} else {
+			counter.innerText = target;
+		}
+	};
+	  updateCount();
+   });
+
+   (function ($) {
+    "use strict";
+    
+    $(".clients-carousel").owlCarousel({
+        autoplay: true,
+        dots: true,
+        loop: true,
+        responsive: { 0: {items: 2}, 768: {items: 4}, 900: {items: 6} }
+    });
+
+    $(".testimonials-carousel").owlCarousel({
+        autoplay: true,
+        dots: true,
+        loop: true,
+        responsive: { 0: {items: 1}, 576: {items: 2}, 768: {items: 3}, 992: {items: 4} }
+    });
+    
+})(jQuery);
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+        $('.back-to-top').fadeIn('slow');
+    } else {
+        $('.back-to-top').fadeOut('slow');
+    }
+});
+$('.back-to-top').click(function () {
+    $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+    return false;
+});
+
+$('.accordion-header').click(function(){
+    $('.accordion .accordion-body').slideUp(500);
+    $(this).next('.accordion-body').slideDown(500);
+    $('.accordion .accordion-header span').text('+');
+    $(this).children('span').text('-');
+});
+
+});
+// Add the provided fade-in and fade-out code here
+document.addEventListener("DOMContentLoaded", function () {
+    const fadeElements = document.querySelectorAll(".fade-element");
+
+    function fadeInOut(element) {
+        element.style.opacity = 0;
+
+        function updateOpacity(opacity) {
+            element.style.opacity = opacity;
+        }
+
+        let opacity = 0;
+        let direction = 1;
+
+        const interval = setInterval(function () {
+            opacity += direction * 0.01;
+            updateOpacity(opacity);
+
+            if (opacity <= 0 || opacity >= 1) {
+                direction *= -1;
+            }
+        }, 50);
+
+        return interval;
+    }
+
+    fadeElements.forEach(function (element) {
+        element.style.opacity = 0;
+        fadeInOut(element);
+    });
+});
+
+function updateCurrentTime() {
+    const timeDiv = document.querySelector('.ion-ios-time[data-target="120"]');
+    const currentTime = new Date();
+    const hours = currentTime.getHours();
+    const minutes = currentTime.getMinutes();
+    const seconds = currentTime.getSeconds();
+    const formattedTime = `${hours}:${minutes}:${seconds}`;
+
+    timeDiv.textContent = formattedTime;
+}
+
+// Update the time immediately and then every second
+updateCurrentTime();
+setInterval(updateCurrentTime, 1000);
+
